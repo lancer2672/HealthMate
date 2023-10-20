89db7485a5e7e4620b33dc84ec0e5bbffac0b147
lancer2672 <jbkhanhtran@gmail.com>
2023-10-20 19:04:41 +0700

    add theme color & untils

--- src/features/theme/index.js
+++ src/features/theme/index.js
@@ -1,33 +1,54 @@
-import {DefaultTheme} from 'react-native-paper';
-
 const darkTheme = {
-  ...DefaultTheme,
-  dark: true,
-  mode: 'adaptive',
-  roundness: 15,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: '#2176FF',
-    accent: '#33A1FD',
-    surface: '#131A26',
-    background: '#131A26',
-    text: '#FFFFFF',
+  primary: '#04a0d7',
+  accent: '#00bbfb',
+  surface: '#131A26',
+  background: '#131A26',
+  text: '#FFFFFF',
+  waterTracking: {
+    primary: '#04a0d7',
+    secondary: '#00bbfb',
   },
 };
 
 const lightTheme = {
-  ...DefaultTheme,
-  dark: false,
-  mode: 'adaptive',
-  roundness: 15,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: '#2176FF',
-    accent: '#33A1FD',
-    surface: '#FFFFFF',
-    background: '#FFFFFF',
-    text: '#000000',
+  primary: '#04a0d7',
+  accent: '#00bbfb',
+  surface: '#FFFFFF',
+  background: '#FFFFFF',
+  text: '#000000',
+  waterTracking: {
+    primary: '#04a0d7',
+    secondary: '#00bbfb',
+    background: '#17354a',
   },
 };
 
 export {darkTheme, lightTheme};
+
+import {ThemeProvider} from 'styled-components/native';
+import {createContext, useState, useEffect} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const ThemeContext = createContext();
+
+export default function ThemeProviderComponent({children}) {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const isUseDarkTheme = await AsyncStorage.getItem('AppTheme');
+
+      if (isUseDarkTheme == 'dark') {
+        setIsDarkTheme(true);
+      }
+    })();
+  }, []);
+
+  return (
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+      <ThemeContext.Provider value={{isDarkTheme: isDarkTheme, setIsDarkTheme}}>
+        {children}
+      </ThemeContext.Provider>
+    </ThemeProvider>
+  );
+}
--- src/utils/tranformData.js
+++ src/utils/tranformData.js
@@ -6,3 +6,13 @@ export const transformUserData = user => {
     avatar: user.avatar ? `${UrlApi}\\${user.avatar}` : null,
   };
 };
+export const transformDrinkProgressData = progress => {
+  const totalAmount = progress.sessions.reduce((acc, item, i) => {
+    return acc + item.amount;
+  }, 0);
+  console.log('totalAmount', totalAmount);
+  return {
+    ...progress,
+    totalAmount,
+  };
+};
