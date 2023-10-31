bd0023f39e5c32836b97d535e085d38154ae1204
lancer2672 <jbkhanhtran@gmail.com>
2023-10-31 16:09:24 +0700

    modify theme, add constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,3 @@
-const DEFAULT_GOAL = 'default_goal';
-const IS_NOTIFICATION_ALLOWED = 'notification_allow';
-
-export {DEFAULT_GOAL, IS_NOTIFICATION_ALLOWED};
+export const DEFAULT_WATER_AMOUNT = 'default_goal';
+export const DEFAULT_STEP = 'default_step';
+export const IS_REMINDING_NOTIFICATION_ALLOWED = 'water_notfication';
